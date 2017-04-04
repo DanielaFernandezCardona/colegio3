45b96f61c42de3ba48e6829d356b0ef92c0c74cb
LAPTOP-16QDN48D\luce <luce28tenis@gmail.com>
2017-04-04 18:24:49 -0500

    correcion

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('./regitroUsuario',function(){
+Route::get('/registroUsuario',function(){
 	 return view('registroUsuario');
 });
 
