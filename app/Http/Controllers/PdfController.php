3653bd54266f8f2fa86c572b32d432532c63250b
LAPTOP-16QDN48D\luce <luce28tenis@gmail.com>
2017-05-06 17:17:10 -0500

    correccion climate code pdf controller

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -33,12 +33,12 @@ return $pdf->stream();
 
 
 // ver informacion empleado
-public function pdf_empleado($id)
+public function pdf_empleado($idEmple)
 {
 $user=DB::table('Empleado')
 ->join('grado', 'grado.idGrado', '=', 'empleado.Grado_idGrado')
 ->select('empleado.documento','empleado.nombre','empleado.apellido', 'empleado.nacionalidad', 'empleado.telefono','empleado.correo','empleado.direccion','empleado.fechaNacimiento','empleado.estudiosRealizados','empleado.nivel','empleado.cargo','empleado.lugarEstudios','empleado.tiempoTrabajo','empleado.fechaIngresoTrabajo','empleado.valorNomina','empleado.estadoCivil','empleado.fechaNacimiento','grado.grado')
-->where('idEmpleado',$id)
+->where('idEmpleado',$idEmple)
 ->first();
 
 //var_dump($user);
@@ -51,7 +51,7 @@ $user=DB::table('Empleado')
             );
 
      $pdf = PDF::loadView('pdfvista',['user' => $user]);
-$paper_size = array(0,0,450,450);
+//$paper_size = array(0,0,450,450);
 
 $pdf->setPaper('A4', 'portrait');
 //$pdf->render();
@@ -63,13 +63,13 @@ return $pdf->download('ResumenInformacion.pdf',$headers);
 }
 
 //imprime un pdf con informacion del estudiante
-public function pdf_estudiante($id)
+public function pdf_estudiante($idEstu)
 {
 $user=DB::table('estudiante')
 ->join('grado', 'grado.idGrado', '=', 'estudiante.idGrado')
 ->join('acudiente', 'idAcudiente', '=', 'estudiante.Acudiente_idAcudiente')
 ->select('estudiante.nombre','estudiante.apellido','estudiante.fechaNac', 'estudiante.documento', 'estudiante.expedicion','estudiante.telefono','estudiante.celular','estudiante.direccion','estudiante.peso','estudiante.tipoSangre','estudiante.anioActual','estudiante.condicion','estudiante.religion','grado.grado','acudiente.documentoPadre','acudiente.nombrePadre','acudiente.nombremadre','acudiente.apellidoMadre','acudiente.apellidoPadre','acudiente.documentoMadre','acudiente.ocupacionPadre','acudiente.ocupacionMadre','acudiente.celularPadre','acudiente.celularMadre','acudiente.correoMadre','acudiente.correoPadre','acudiente.estadoCivil','acudiente.nombreAcu','acudiente.documentoAcu','acudiente.celularAcu','acudiente.ocupacion','acudiente.correoAcu','acudiente.parentesco')
-->where('idEstudiante',$id)
+->where('idEstudiante',$idEstu)
 ->first();
 
 $headers = array(
