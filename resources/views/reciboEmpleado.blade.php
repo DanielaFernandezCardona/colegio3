523781640a6c34cb5ed5815fb68caef91256d8e5
jhonMovil <ramirescortes@hotmail.com>
2017-05-23 05:44:34 -0500

    caso 3
    caso3

--- resources/views/reciboEmpleado.blade.php
+++ resources/views/reciboEmpleado.blade.php
@@ -167,8 +167,10 @@ return true;
                     <li class="dropdown pull-right">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown"><svg class="glyph stroked male-user"><use xlink:href="#stroked-male-user"></use></svg>{{Auth::user()->nombre}} <span class="caret"></span></a>
                       
-                            
+                          <ul class="dropdown-menu" role="menu">
                             <li><a href="/salir"><svg class="glyph stroked cancel"><use xlink:href="#stroked-cancel"></use></svg> Salir</a></li>
+                        </ul>
+
                         </ul>
                     </li>
                 </ul>
